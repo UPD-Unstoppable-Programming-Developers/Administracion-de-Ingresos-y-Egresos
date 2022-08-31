498d4d89ad8618d09cbda55ed625d17e2ad68657
Miguel Angel Diaz D <migerodd888@gmail.com>
2022-08-31 16:20:14 -0500

    Practicando extensiones

--- Employee.java
+++ Employee.java
@@ -1,44 +1,48 @@
-// Es posible crear una nueva instancia de la clase "Empleado"
-// Es posible leer y modificar el nombre de un empleado
-// Es posible leer y modificar el correo de un empleado
-// Es posible leer y modificar la empresa a la que el empleado pertenece
-// Es posible leer y modificar el rol del empleado (administrador, operativo)
 public class Employee {
-// Attributes
+    // Attributes
     private String Email;
     private String Name;
     private String Enterprise;
     private String Role;
-// Builder
+
+    // Builder
     public Employee(String Email, String Name, String Enterprise, String Role) {
         this.Email = Email;
         this.Name = Name;
         this.Enterprise = Enterprise;
         this.Role = Role;
     }
-// Getters and Setters 
+
+    // Getters and Setters
     public String getEmail() {
         return Email;
     }
+
     public void setEmail(String Email) {
         this.Email = Email;
     }
+
     public String getName() {
         return Name;
     }
+
     public void setName(String Name) {
         this.Name = Name;
     }
+
     public String getEnterprise() {
         return Enterprise;
     }
+
     public void setEnterprise(String Enterprise) {
         this.Enterprise = Enterprise;
     }
+
     public String getRole() {
         return Role;
     }
+
     public void setRole(String Role) {
         this.Role = Role;
-    } 
+    }
 }
--- Enterprise.java
+++ Enterprise.java
@@ -1,25 +1,23 @@
-// Es posible crear una instancia de la clase "Enterprise"
-// Es posible leer y modificar el nombre de la empresa
-// Es posible leer y modificar la dirección de la empresa
-// Es posible leer y modificar el teléfono de la empresa
-// Es posible leer y modificar el NIT de la empresa
 public class Enterprise {
-// Attributes
+    // Attributes
     private String name;
     private String address;
     private String phone;
     private String NIT;
-// Builder
+
+    // Builder
     public Enterprise(String name, String address, String phone, String NIT) {
         this.name = name;
         this.address = address;
         this.phone = phone;
         this.NIT = NIT;
     }
-// Getters & Setters
+
+    // Getters & Setters
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -27,6 +25,7 @@ public class Enterprise {
     public String getAddress() {
         return address;
     }
+
     public void setAddress(String address) {
         this.address = address;
     }
@@ -34,6 +33,7 @@ public class Enterprise {
     public String getPhone() {
         return phone;
     }
+
     public void setPhone(String phone) {
         this.phone = phone;
     }
@@ -41,6 +41,7 @@ public class Enterprise {
     public String getNIT() {
         return NIT;
     }
+
     public void setNIT(String NIT) {
         this.NIT = NIT;
     }
