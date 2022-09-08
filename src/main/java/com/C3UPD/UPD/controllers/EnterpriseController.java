a4a9ee7c451b067420bb1447579903e6c76d19ee
Miguel Angel Diaz D <migerodd888@gmail.com>
2022-09-07 20:08:01 -0500

    dar formato a los archivos

--- src/main/java/com/C3UPD/UPD/controllers/EnterpriseController.java
+++ src/main/java/com/C3UPD/UPD/controllers/EnterpriseController.java
@@ -12,37 +12,38 @@ import java.util.List;
 public class EnterpriseController {
     EnterpriseService service;
     Enterprise enterprise;
-    //List<Enterprise> enterprises;
-    //enterprises = new List<Enterprise>();
+    // List<Enterprise> enterprises;
+    // enterprises = new List<Enterprise>();
 
     public EnterpriseController() {
         this.service = new EnterpriseService();
 
     }
+
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("hello %s!", name);
     }
+
     @GetMapping("/enterprise")
-    public List<Enterprise> getEnterprises(){
+    public List<Enterprise> getEnterprises() {
         return service.getEnterprises();
     }
 
     @PostMapping("/enterprise")
-    public ResponseEntity<Enterprise> create(@RequestBody Enterprise newEnterprise){
+    public ResponseEntity<Enterprise> create(@RequestBody Enterprise newEnterprise) {
         Enterprise enterprise = service.addEnterprise(newEnterprise);
 
-        //this.enterprise1 = new Enterprise();
-        //'enterprise1.setName("Empresa 123");
-        //service.setEnterprise(enterprise1);
+        // this.enterprise1 = new Enterprise();
+        // 'enterprise1.setName("Empresa 123");
+        // service.setEnterprise(enterprise1);
         return new ResponseEntity<>(enterprise, HttpStatus.CREATED);
     }
 
     @GetMapping("/enterprise/{id}")
-    public Enterprise enterpriseGetId(@PathVariable("id") String id){
+    public Enterprise enterpriseGetId(@PathVariable("id") String id) {
         return service.getEnterpriseID(Integer.parseInt(id));
     }
-
 
 
 }
