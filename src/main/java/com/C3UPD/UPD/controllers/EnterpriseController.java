b88465450def6a37fcb8d29f1cb2a927b6ab6280
Juan Hurtado <juandahur@gmail.com>
2022-09-09 22:30:42 -0500

    Agregar Delete mapping a Enterprise

--- src/main/java/com/C3UPD/UPD/controllers/EnterpriseController.java
+++ src/main/java/com/C3UPD/UPD/controllers/EnterpriseController.java
@@ -3,9 +3,7 @@ package com.C3UPD.UPD.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 // Import Functions
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 // Import Models and Services
 import com.C3UPD.UPD.Models.Enterprise;
 import com.C3UPD.UPD.services.EnterpriseService;
@@ -21,7 +19,11 @@ public class EnterpriseController {
     private ResponseEntity<List<Enterprise>> listAllEnterprise (){
         return ResponseEntity.ok(enterpriseService.getAllEnterprise());
 }
-
+    @DeleteMapping
+    private ResponseEntity<Void> deleteEnterprise (@RequestBody Enterprise enterprise){
+        enterpriseService.delete(enterprise);
+        return ResponseEntity.ok().build();
+    }
 
     /* 
     EnterpriseService Service;
