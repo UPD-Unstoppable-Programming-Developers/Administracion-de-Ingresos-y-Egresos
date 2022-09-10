eb8f41018f422ca17a408fb0d7c8c8e3fd94ece2
Juan Hurtado <juandahur@gmail.com>
2022-09-09 23:10:15 -0500

    Corrección de errores

--- src/main/java/com/C3UPD/UPD/repository/TransactionRespository.java
+++
@@ -1,7 +0,0 @@
-package com.C3UPD.UPD.repository;
-
-import com.C3UPD.UPD.Models.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRespository extends JpaRepository<Transaction,Long> {
-}
