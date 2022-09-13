a7b9b62ca3d042ff0321a97f9c0163e769c22b83
Miguel Angel Diaz D <migerodd888@gmail.com>
2022-09-13 17:22:08 -0500

    ordenar

--- src/main/java/com/C3UPD/UPD/Models/Enterprise.java
+++ src/main/java/com/C3UPD/UPD/Models/Enterprise.java
@@ -1,7 +1,6 @@
 package com.C3UPD.UPD.Models;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 import javax.persistence.*;
 import java.util.List;
 
@@ -9,7 +8,7 @@ import java.util.List;
 @Table(name = "Enterprise")
 public class Enterprise {
     @Id
-    //@GeneratedValue(strategy = GenerationType.AUTO)
+    // @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @Column(name = "name")
     private String name;
@@ -27,11 +26,12 @@ public class Enterprise {
     @OneToMany(mappedBy = "enterprise")
     private List<Employee> employeeList;
 
-    public Enterprise(){
+    public Enterprise() {
 
     }
 
-    public Enterprise(Long id, String name, String address, String phone, String NIT, List<Transaction> transactionList, List<Employee> employeeList) {
+    public Enterprise(Long id, String name, String address, String phone, String NIT, List<Transaction> transactionList,
+            List<Employee> employeeList) {
         this.id = id;
         this.name = name;
         this.address = address;
@@ -42,9 +42,6 @@ public class Enterprise {
     }
 
     // Getters and setters
-
-
-
 
     public Long getId() {
         return id;
@@ -85,6 +82,7 @@ public class Enterprise {
     public void setNIT(String NIT) {
         this.NIT = NIT;
     }
+
     @JsonManagedReference
     public List<Transaction> getTransactionList() {
         return transactionList;
@@ -93,6 +91,7 @@ public class Enterprise {
     public void setTransactionList(List<Transaction> transactionList) {
         this.transactionList = transactionList;
     }
+
     @JsonManagedReference
     public List<Employee> getEmployeeList() {
         return employeeList;
--- src/main/java/com/C3UPD/UPD/services/EnterpriseService.java
+++ src/main/java/com/C3UPD/UPD/services/EnterpriseService.java
@@ -1,11 +1,9 @@
 package com.C3UPD.UPD.services;// Import Models
+
 import com.C3UPD.UPD.Models.Enterprise;
 import com.C3UPD.UPD.repository.EnterpriseRepository;
-
-// Import Special Applications
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,25 +12,25 @@ public class EnterpriseService {
 
     @Autowired
     private EnterpriseRepository enterpriseRepository;
-    public Enterprise create(Enterprise enterprise){
+
+    public Enterprise create(Enterprise enterprise) {
 
         return enterpriseRepository.save(enterprise);
     }
 
-    public List<Enterprise> getAllEnterprise(){
+    public List<Enterprise> getAllEnterprise() {
 
         return enterpriseRepository.findAll();
     }
 
-    public void delete (Enterprise enterprise){
+    public void delete(Enterprise enterprise) {
 
         enterpriseRepository.delete(enterprise);
     }
 
-    public Optional<Enterprise> findById(Long id){
+    public Optional<Enterprise> findById(Long id) {
 
         return enterpriseRepository.findById(id);
     }
-
 
 }
